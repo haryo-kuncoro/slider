8a59b03f0898cc43d6cd1111117ec0853e94a953
haryo-kuncoro <haryorawzikers95@gmail.com>
2025-10-28 16:26:10 +0700

    update database

--- db_slider.sql
+++ db_slider.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 13/10/2025 12:32:36
+ Date: 28/10/2025 16:25:43
 */
 
 SET NAMES utf8mb4;
@@ -38,14 +38,15 @@ CREATE TABLE `tbl_wisudawan`  (
   `gelombang` varchar(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_gelombang_prodi`(`gelombang` ASC, `prodi` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3933 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
+) ENGINE = InnoDB AUTO_INCREMENT = 3935 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of tbl_wisudawan
 -- ----------------------------
-INSERT INTO `tbl_wisudawan` VALUES (3604, 1, '2021020080', 'Wisudawan 01', 'Ayah 01', 'Ibu 01', 'Medan / 25 Februari 1995', NULL, NULL, '3,69', 'Sistem Pendukung Keputusan untuk Menentukan Pemberian Reward Pemasok Terbaik Pada PT. Midi Utama Indonesia Tbk Menggunakan Metode Moosra', 'Cumlaude', 'Hukum', '1');
-INSERT INTO `tbl_wisudawan` VALUES (3605, 2, '2021020275', 'Wisudawan 02', 'Ayah 02', 'Ibu 02', 'Pesununan / 13 April 2002', NULL, NULL, '3,73', 'Penerapan Tanda Tangan Digital Menggunakan Algoritma SHA-1 Untuk Meningkatkan Keamanan Dan Integritas Bukti Pembayaran uang Sekolah Di Perguruan Kristen Methodist Indonesia.', 'Cumlaude', 'Hukum', '1');
-INSERT INTO `tbl_wisudawan` VALUES (3606, 3, '2021020128', 'Wisudawan 03', 'Ayah 03', 'Ibu 03', 'Kuta Rimbaru / 19 Desember 2001', NULL, NULL, '3,59', 'Penerapan Data Mining dalam Segmentasi Kebutuhan Belanja Anggota Koperasi Karyawan Budi Murni Medan Menggunakan Metode K-Nearest Neighbor.', 'Cumlaude', 'Manajemen', '1');
-INSERT INTO `tbl_wisudawan` VALUES (3607, 4, '2021020006', 'Wisudawan 04', 'Ayah 04', 'Ibu 04', 'Medan / 02 Januari 2001', NULL, NULL, '3,74', 'Analisis Perbandingan Metode Canny, Sobel dan Laplacian of Gaussian (LoG) dalam Mendeteksi Tepi Logo Provinsi di Indonesia', 'Cumlaude', 'Akuntansi', '1');
+INSERT INTO `tbl_wisudawan` VALUES (3604, 1, '2021020080', 'Wisudawan 01', 'Ayah 01', 'Ibu 01', 'Medan / 25 Februari 1995', NULL, NULL, NULL, 'Sistem Pendukung Keputusan untuk Menentukan Pemberian Reward Pemasok Terbaik Pada PT. Midi Utama Indonesia Tbk Menggunakan Metode Moosra', 'Cumlaude', 'Hukum', '1');
+INSERT INTO `tbl_wisudawan` VALUES (3605, 2, '2021020275', 'Wisudawan 02', 'Ayah 02', 'Ibu 02', 'Pesununan / 13 April 2002', NULL, NULL, NULL, 'Penerapan Tanda Tangan Digital Menggunakan Algoritma SHA-1 Untuk Meningkatkan Keamanan Dan Integritas Bukti Pembayaran uang Sekolah Di Perguruan Kristen Methodist Indonesia.', 'Cumlaude', 'Hukum', '1');
+INSERT INTO `tbl_wisudawan` VALUES (3606, 1, '2021020128', 'Wisudawan 03', 'Ayah 03', 'Ibu 03', 'Kuta Rimbaru / 19 Desember 2001', '', '', NULL, 'Penerapan Data Mining dalam Segmentasi Kebutuhan Belanja Anggota Koperasi Karyawan Budi Murni Medan Menggunakan Metode K-Nearest Neighbor.', 'Cumlaude', 'Manajemen', '1');
+INSERT INTO `tbl_wisudawan` VALUES (3607, 1, '2021020006', 'Wisudawan 04', 'Ayah 04', 'Ibu 04', 'Medan / 02 Januari 2001', '', '', NULL, 'Analisis Perbandingan Metode Canny, Sobel dan Laplacian of Gaussian (LoG) dalam Mendeteksi Tepi Logo Provinsi di Indonesia', 'Cumlaude', 'Akuntansi', '1');
+INSERT INTO `tbl_wisudawan` VALUES (3934, 2, '2021020054', 'Wisudawan 05', 'Ayah 05', 'Ibu 05', 'Medan / 02 Januari 2001', 'Medan', 'Medan', '', 'Dampak Implementasi Enterprise Resource Planning (ERP) Terhadap Efisiensi Proses Bisnis Pada PT. Maju Bersama', 'Sangat Memuaskan', 'Akuntansi', '1');
 
 SET FOREIGN_KEY_CHECKS = 1;
