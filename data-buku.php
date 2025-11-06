94f71048e1600ea4be045697e84dc8f3ad1447a8
haryo-kuncoro <haryorawzikers95@gmail.com>
2025-11-06 19:11:00 +0700

    urut buku by urutan

--- data-buku.php
+++ data-buku.php
@@ -19,7 +19,7 @@ $data_final = [];
 foreach ($array_urutan as $prodiforshow) {
     $tableprodi = "tbl_wisudawan";
     if ($tableprodi != "") {
-        $q = "SELECT * FROM $tableprodi WHERE prodi='".$prodiforshow."' ORDER BY nama ASC";
+        $q = "SELECT * FROM $tableprodi WHERE prodi='".$prodiforshow."' ORDER BY urutan ASC";
         $res = mysqli_query($koneksi, $q);
         while ($r = mysqli_fetch_assoc($res)) {
             $NOURUT = strtoupper($r['urutan'] ?? '');
